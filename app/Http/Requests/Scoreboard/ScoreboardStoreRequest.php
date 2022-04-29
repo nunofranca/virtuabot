de183bd5cfbe053f0f328336933035ba1d7e149b
Nuno França <alberto.oliveira.ext@dasa.com.br>
2022-04-29 15:29:38 -0300

    ..

--- app/Http/Requests/Scoreboard/ScoreboardStoreRequest.php
+++ app/Http/Requests/Scoreboard/ScoreboardStoreRequest.php
@@ -13,7 +13,7 @@ class ScoreboardStoreRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,8 @@ class ScoreboardStoreRequest extends FormRequest
     public function rules()
     {
         return [
-            'result' => 'string|required'
+            'result' => 'string|required',
+            'leagueHour' => 'string|required'
         ];
     }
 }
