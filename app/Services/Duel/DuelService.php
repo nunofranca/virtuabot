393d3a73d4364b11289f824fe3f9f4f9731a4627
Nuno França <alberto.oliveira.ext@dasa.com.br>
2022-04-29 21:19:16 -0300

    ..

--- app/Services/Duel/DuelService.php
+++ app/Services/Duel/DuelService.php
@@ -3,14 +3,19 @@
 namespace App\Services\Duel;
 
 use App\Repositories\Duel\DuelRepositoryInterface;
+use App\Services\ApiTelegram\TelegramService;
+use App\Services\ApiTelegram\TelegramServiceInterface;
+use TelegramBot\Api\BotApi;
 
 class DuelService implements DuelServiceInterface
 {
     private DuelRepositoryInterface $duelRepository;
+    private TelegramServiceInterface $telegramService;
 
-    public function __construct(DuelRepositoryInterface $duelRepository)
+    public function __construct(DuelRepositoryInterface $duelRepository, TelegramServiceInterface $telegramService)
     {
         $this->duelRepository = $duelRepository;
+        $this->telegramService = $telegramService;
     }
 
     public function getAll()
@@ -20,7 +25,11 @@ class DuelService implements DuelServiceInterface
 
     public function create($attributes)
     {
-        return $this->duelRepository->create($attributes);
+        $this->duelRepository->create($attributes);
+
+        return $this->telegramService->sendMessage('Duelo Cadastrado - '. $attributes['hour']);
+
+
     }
 
     public function find($id)
